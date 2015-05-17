a003dcfe4d748bebc1fa0e48be8181cc21e3cd0d
Krister Viirsaar <krister.viirsaar@gmail.com>
2015-05-17 19:53:46 +0300

    scrolling

--- view/chatBubbles.js
+++ view/chatBubbles.js
@@ -3,7 +3,15 @@ Template.chatBubbles.helpers({
 		return Roles.userIsInRole(Meteor.userId(), 'admin')
 	},
 	chatBubbles: function() {
-		return ChatBubblesCollection.find()
+		var find = ChatBubblesCollection.find()
+		find.observeChanges({
+			changed: function (id, fields) {
+				Meteor.setTimeout(function(){
+					$('.chatBubbles-bubble[data-chat-id="'+id+'"] .chatBubbles-msgs-container').scrollTop(9999)
+				}, 10)
+			},
+		})
+		return find
 	},
 	authorAdmin: function() {
 		return this.role == 'admin'
@@ -13,7 +21,8 @@ Template.chatBubbles.helpers({
 Template.chatBubbles.events({
 	'submit form[name="chatBubblesNewMessage"]': function (e) {
 		e.preventDefault()
-		var chatId = $(e.currentTarget).parents('.chatBubbles-bubble[data-chat-id]').first().attr('data-chat-id')
+		var chat = $(e.currentTarget).parents('.chatBubbles-bubble[data-chat-id]').first()
+		var chatId = chat.attr('data-chat-id')
 		var msg = $(e.currentTarget).find('input[name="msg"]').val()
 		$(e.currentTarget).find('input[name="msg"]').val("")
 
@@ -22,6 +31,9 @@ Template.chatBubbles.events({
 			authorId: Meteor.userId(),
 			role: Roles.userIsInRole(Meteor.userId(), 'admin') ? 'admin' : 'guest',
 		}
-		ChatBubblesCollection.update(chatId, {$push: {messages: data}})
+		ChatBubblesCollection.update(chatId, {$push: {messages: data}}, function(){
+			chat.find('.chatBubbles-msgs-container').scrollTop(9999)
+		})
+
 	}
 });
